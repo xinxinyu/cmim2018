f49dd49d3c6809ad732c221f408a880a3926179b
xinxin <35332781+xinxinyu@users.noreply.github.com>
2018-01-25 17:09:19 +0200

    About 2D trapz

---
+++ task01/main2D.m
@@ -0,0 +1,21 @@
+clc
+close all
+clear variables
+
+
+low_limitX=-3;
+up_limitX=3;
+low_limitY=-5;
+up_limitY=5;
+no_splits=100;
+
+
+ result = integral_trapezoid(@(y)(integral_trapezoid(@(x)(-x^4-y^4),low_limitX, up_limitX,no_splits)),low_limitY, up_limitY,no_splits);
+
+
+x=-3:6/100:3;
+y=-5:10/100:5;
+[X,Y]=meshgrid(x,y);
+fun=-X.^4-Y.^4;
+I = trapz(y,trapz(x,fun,2));
+surf(X,Y,fun)
