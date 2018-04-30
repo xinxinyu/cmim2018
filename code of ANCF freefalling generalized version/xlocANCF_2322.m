88a42c2a4b615899da18b2213d955c7d6f7d7d43
xinxin <35332781+xinxinyu@users.noreply.github.com>
2018-04-30 12:21:53 +0300

    zero

--- code of ANCF freefalling generalized version/xlocANCF_2322.m
+++
@@ -1,9 +0,0 @@
-function loc = xlocANCF_2322(nodes,comps)
-% make location vector in x from nodelist and componentlist
-
-loc = [];
-for n=1:length(nodes)
-  nn = nodes(n);
-  loc = [loc 4*nn-4+comps];
-end
-  
--- code of ANCF freefalling generalized version/xlocANCF_3333.m
+++
@@ -1,9 +0,0 @@
-function loc = xlocANCF_3333(nodes,comps)
-% make location vector in x from nodelist and componentlist
-
-loc = [];
-for n=1:length(nodes)
-  nn = nodes(n);
-  loc = [loc 9*nn-9+comps];
-end
-  
